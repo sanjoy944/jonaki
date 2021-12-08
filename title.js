00388c94ac1c0281796a66a960fdf8b108b974a8
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2021-12-08 19:26:46 +0600

    Add files via upload

---
+++ title.js
@@ -0,0 +1,11 @@
+function keygen() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/fzroB0634a5513029319feaccd2995b01b05088824fbf?q=<b:eval expr='data:blog.pageTitle'/>.keyfile&quot;, &quot;_blank&quot;,);
+}
+
+function Crack() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/fzroB0634a5513029319feaccd2995b01b05088824fbf?q=<b:eval expr='data:blog.pageTitle'/>.Crackfile&quot;, &quot;_blank&quot;,);
+}
+
+function setup() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/fzroB0634a5513029319feaccd2995b01b05088824fbf?q=<b:eval expr='data:blog.pageTitle'/>.Crack.Setup.file&quot;, &quot;_blank&quot;,);
+}
