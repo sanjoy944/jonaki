3fb5b5b16df798d84d59f0ba931320eb562a59e8
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2022-01-14 16:38:29 +0600

    Add files via upload

---
+++ 112233.js
@@ -0,0 +1,16 @@
+function keygen() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.keyfile&quot;, &quot;_blank&quot;,);
+}
+
+function Crack() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.Crackfile&quot;, &quot;_blank&quot;,);
+}
+
+function setup() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.Crack.Setup.file&quot;, &quot;_blank&quot;,);
+}
+
+
+function password() {
+  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=All+File+Password+Download &quot;, &quot;_blank&quot;,);
+}
