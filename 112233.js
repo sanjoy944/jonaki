fc7dea3ee6aa342e8ec804779946a9e694886934
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2022-01-14 16:47:47 +0600

    Update 112233.js

--- 112233.js
+++ 112233.js
@@ -1,16 +1,16 @@
 function keygen() {
-  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.keyfile&quot;, &quot;_blank&quot;,);
+  window.open("https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.keyfile", "_blank",);
 }
 
 function Crack() {
-  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.Crackfile&quot;, &quot;_blank&quot;,);
+  window.open("https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.Crackfile", "_blank",);
 }
 
 function setup() {
-  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.Crack.Setup.file&quot;, &quot;_blank&quot;,);
+  window.open("https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=<b:eval expr='data:blog.pageTitle'/>.Crack.Setup.file", "_blank",);
 }
 
 
 function password() {
-  window.open(&quot;https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=All+File+Password+Download &quot;, &quot;_blank&quot;,);
+  window.open("https://href.li/?https://downloadgate.org/h3rwR09d84764fdf97583db0a88e8482beb8c9717d30c?q=All+File+Password+Download ", "_blank",);
 }
