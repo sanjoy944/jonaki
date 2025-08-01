801268934269811c759bad74bd6192ac27daf85d
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2025-08-01 18:43:37 +0600

    floating-button.js

--- floating-button.js
+++ floating-button.js
@@ -54,6 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const tools = [
         {class: 'c11', name: 'OCR বাংলা', url: 'https://bangla-ocr-online.blogspot.com/'},
         {class: 'c4', name: 'Unicode2Bijoy', url: 'https://unicode-bijoy.blogspot.com/'},
+        {class: 'c1', name: 'QR Generator', url: 'https://qrcode-generator-pro.blogspot.com/'},
         {class: 'c6', name: 'PDF Add Watermark', url: 'https://pdf-add-watermark.blogspot.com/'},
         {class: 'c12', name: 'OCR English', url: 'https://english-ocr-online.blogspot.com/'},
         {class: 'c8', name: 'PDF Split-Merge', url: 'https://pdf-split-merge-tool.blogspot.com/'},
