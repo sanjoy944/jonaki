686fc2b4c8defcbfec24e9bc58f0f5995ec4f453
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2026-03-21 09:37:00 +0600

    floating-button.js

--- floating-button.js
+++ floating-button.js
@@ -59,6 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
        {class: 'c12', name: 'OCR Bangla', url: 'https://bangla-ocr-online.blogspot.com/'},
          {class: 'c7', name: 'Signature BG Remover', url: 'https://signaturebgremover.blogspot.com/'},
          {class: 'c11', name: 'Video Snapshot', url: 'https://video-snapshot-manager.blogspot.com/'},
+         {class: 'c3', name: 'Data To Word', url: 'https://datetowordsconverter.blogspot.com/'},
         {class: 'c6', name: 'PDF Add Watermark', url: 'https://pdf-add-watermark.blogspot.com/'},
         {class: 'c12', name: 'OCR English', url: 'https://english-ocr-online.blogspot.com/'},
         {class: 'c8', name: 'PDF Split-Merge', url: 'https://pdf-split-merge-tool.blogspot.com/'},
