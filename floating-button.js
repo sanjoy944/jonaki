0cfc10bfdf6749e184d909bd7d69303801d1ccb3
sanjoy944 <44388753+sanjoy944@users.noreply.github.com>
2026-03-21 16:27:03 +0600

    floating-button.js

--- floating-button.js
+++ floating-button.js
@@ -65,6 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
           {class: 'c4', name: 'Age Calculator', url: 'https://age-calculator94.blogspot.com/'},
         {class: 'c6', name: 'PDF Add Watermark', url: 'https://pdf-add-watermark.blogspot.com/'},
         {class: 'c12', name: 'OCR English', url: 'https://english-ocr-online.blogspot.com/'},
+         {class: 'c9', name: 'Teletalk Photo 300px', url: 'https://teletalkphotosignatureresizer.blogspot.com/'},
         {class: 'c8', name: 'PDF Split-Merge', url: 'https://pdf-split-merge-tool.blogspot.com/'},
         {class: 'c2', name: 'PDF2JPG HQ', url: 'https://pdf-to-jpg-converter-hq.blogspot.com/'},
          {class: 'c4', name: 'Video2PDF', url: 'https://videotopdf.blogspot.com/'},
